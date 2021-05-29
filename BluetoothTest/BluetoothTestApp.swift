dae0391db7629e0b55ad6c731e0a6d34b89c014c
Nick Seel <nick.seel@gmail.com>
2021-05-29 13:41:51 -0500

    Initial Commit

---
+++ BluetoothTest/BluetoothTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothTestApp.swift
+//  BluetoothTest
+//
+//  Created by Nick Seel on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
